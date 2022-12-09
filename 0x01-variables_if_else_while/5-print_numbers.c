cf997361e2863c5eb6b45d8f6c18718335c36573
ajia-code <akomst6@yahoo.com>
2022-12-09 13:58:17 -0800

    corrected coding style to betty's

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints numbers from 0 to 9.
  *
@@ -7,12 +6,11 @@
  */
 int main(void)
 {
-  int i;
-
-  for (i = 0; i < 10; i++)
-    {
-      printf("%d", i);
-    }
-  putchar('\n');
-  return (0);
-} 
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
