d0f33368b9aab4923c8a5cb70214f3bb7703e5f8
Ajia-codes <akomst6@yahoo.com>
2022-12-22 21:24:08 +0100

    Added corrections to 1-strncat.c file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,23 +8,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int count = 0, count2  = 0, count3 = 0;
+int count = 0, count2 = 0;
+
 while (*(dest + count) != '\0')
 {
 count++;
 }
 
-while (*(src + count3) != '\0')
+while (count2 < n)
 {
-count3++;
-}
-
-for (count2; n > count2; count2++)
-{
-if (count2 == count3 )
-break;
 *(dest + count) = *(src + count2);
+if (*(src + count2) == '\0')
+break;
 count++;
+count2++;
 }
 return (dest);
 }
