54e17f0e98af559d6cf7873ee3644c240d2cb1e2
Ajia-codes <akomst6@yahoo.com>
2022-12-22 21:42:19 +0100

    Added correction  to 3-strncmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,17 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-  int n ;
-  for (n = 0; n >= 0 && s1[n] != '\0' && s2[n] != '\0'; n++)
-    {
-      int subt = s1[n] - s2[n];
-      if (subt == 0)
-	{
-	  continue;
-	}
-      else
-	{
-	  return(subt);
-	}
-    }
+int i = 0, op = 0;
+
+while (op == 0)
+{
+if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+break;
+op = *(s1 + i) - *(s2 + i);
+i++;
+}
+
+return (op);
 }
