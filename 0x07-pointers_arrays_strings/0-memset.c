f1d04139a4b6b6aad1b5bacf8e801a3d3398e03e
Ajia-codes <akomst6@yahoo.com>
2023-01-02 13:05:20 +0100

    editted 0-memset.c file to follow Betty syntax

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,10 +10,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned c;
-  for (c = 0; c < n; c++)
-    {
-      s[c] = b;
-    }
-  return (s)
+unsigned c;
+for (c = 0; c < n; c++)
+{
+s[c] = b;
+}
+return (s)
 }
