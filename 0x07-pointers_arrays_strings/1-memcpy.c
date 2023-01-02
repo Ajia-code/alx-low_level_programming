ed123d2b23970cf5c9e6650aa96f8e88b6df7cd2
Ajia-codes <akomst6@yahoo.com>
2023-01-02 13:42:46 +0100

    editted 1-memcpy to follow Betty stye

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,10 +9,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  unsigned int c;
-  for (c = 0; c < n; c++)
-    {
-      dest[c] = src[c];
-    }
-  return (dest);
+unsigned int c;
+for (c = 0; c < n; c++)
+{
+dest[c] = src[c];
+}
+return (dest);
 }
