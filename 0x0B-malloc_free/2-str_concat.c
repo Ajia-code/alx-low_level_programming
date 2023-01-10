98f77ca4a17fac0899e3ff7232a8da794055d7af
Ajia-codes <akomst6@yahoo.com>
2023-01-10 10:31:44 +0100

    corrected 2-str_concat.c file to clear compilation Betty stylcoerrors

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,14 +2,14 @@
 
 char *str_concat(char *s1, char *s2)
 {
-unsigned int b, c, y;
+unsigned int b, c, i, y;
+char *concat;
 
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
  
-
 for (b = 0; s1[b] != '\0'; b++)
 {
 ;
@@ -19,16 +19,15 @@ for (c = 0; s2[c] != '\0'; c++)
 ;
 }
 
-char *concat;
-concat  = (char *) malloc (sizeof (char) * (b + c + 1));
+concat  = (char *) malloc(sizeof (char) * (b + c + 1));
 
 if (strout == NULL)
 {
-free(strout);
+free(concat);
 return (NULL);
 }
 
-for (int i = 0; s1[i] != '\0'; i++)
+for (i = 0; s1[i] != '\0'; i++)
 {
 concat[i] = s1[i];
 }
