ce0e8f88cd75094c98e456c6863f624ba286ce1a
ajia-code <akomst6@yahoo.com>
2023-01-13 12:43:29 -0800

    added 101-mul.c file and added betty style to 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,21 +22,15 @@ if (s2 == NULL)
 s2 = "";
 
 for (k = 0; s1[k] != '\0'; k++)
-{
 ;
-}
 
 for (j = 0; s2[j] != '\0'; j++)
-{
 ;
-}
 
 if (n > j)
-{
 n = j;
-}
-size = k + n;
 
+size = k + n;
 str = malloc(size + 1);
 
 if (str == NULL)
@@ -45,15 +39,10 @@ return (NULL);
 for (i = 0; i < size; i++)
 {
 if (i < k)
-{
 str[i] = s1[i];
-}
 else
-{
 str[i] = s2[i - k];
 }
-}
 str[i] = '\0';
-
 return (str);
 }
